9515c966bd001d00e7bb2cbef491683f3850400f
Jay Wick <developer@jay-wick.com>
2014-10-12 14:24:08 +1100

    Numeric, Check, Enum and Data list PropertyViews

--- Scrumr/Views/PropertyView.cs
+++ Scrumr/Views/PropertyView.cs
@@ -37,7 +37,19 @@ namespace Scrumr
 
         public static PropertyView Create(PropertyItem propertyItem)
         {
-            return new TextPropertyView(propertyItem);
+            if (propertyItem.Type == typeof(string))
+                return new TextPropertyView(propertyItem);
+
+            if (propertyItem.Type == typeof(int) || propertyItem.Type == typeof(double) || propertyItem.Type == typeof(float) || propertyItem.Type == typeof(decimal))
+                return new NumericPropertyView(propertyItem);
+
+            if (propertyItem.Type == typeof(bool))
+                return new CheckPropertyView(propertyItem);
+
+            if (propertyItem.Type == typeof(Enum))
+                return new EnumListPropertyView(propertyItem);
+
+            return null;
         }
     }
 
@@ -55,6 +67,86 @@ namespace Scrumr
         public override object Value
         {
             get { return (View as TextBox).Text; }
+        }
+    }
+
+    public class NumericPropertyView : PropertyView
+    {
+        public NumericPropertyView(PropertyItem propertyItem)
+            : base(propertyItem)
+        {
+            View = new TextBox
+            {
+                Text = propertyItem.Value.ToString(),
+            };
+
+            View.PreviewTextInput += (s, e) =>
+            {
+                var pattern = @"^"          // start of string
+                            + @"[-+]?"      // optional plus or minus sign
+                            + @"[0-9]*"     // 0 or more digits
+                            + @"\.?"        // optional decimal point
+                            + @"[0-9]+"     // 1 or more digits
+                            + @"$";         // end of string
+
+                e.Handled = new Regex(pattern).IsMatch(e.Text);
+            };
+        }
+
+        public override object Value
+        {
+            get { return Convert.ToDouble((View as TextBox).Text); }
+        }
+    }
+
+    public class CheckPropertyView : PropertyView
+    {
+        public CheckPropertyView(PropertyItem propertyItem)
+            : base(propertyItem)
+        {
+            View = new CheckBox
+            {
+                IsChecked = Convert.ToBoolean(propertyItem.Value),
+            };
+        }
+
+        public override object Value
+        {
+            get { return (View as CheckBox).IsChecked; }
+        }
+    }
+
+    public class EnumListPropertyView : PropertyView
+    {
+        public EnumListPropertyView(PropertyItem propertyItem)
+            : base(propertyItem)
+        {
+            View = new ListBox
+            {
+                ItemsSource = Enum.GetNames(propertyItem.Type).ToList(),
+            };
+        }
+
+        public override object Value
+        {
+            get { return Enum.Parse(Property.Type, (View as ListBox).SelectedItem.ToString()); }
+        }
+    }
+
+    public class DataListPropertyView : PropertyView
+    {
+        public DataListPropertyView(PropertyItem propertyItem)
+            : base(propertyItem)
+        {
+            View = new ListBox
+            {
+                ItemsSource = null, //todo
+            };
+        }
+
+        public override object Value
+        {
+            get { return null; } //todo
         }
     }
 }
