9d13122800cf075a632e3f06ebe11b879e463fd2
Jay Wick <developer@jay-wick.com>
2014-11-20 22:39:08 +1100

    Default backlog and feature per project

--- Scrumr/Database/Entities/Project.cs
+++ Scrumr/Database/Entities/Project.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,17 @@ namespace Scrumr
     {
         [IgnoreRender]
         public long NextProjectTicketId { get; set; }
+
+        [IgnoreRender]
+        public long BacklogId { get; set; }
+
+        [IgnoreRender]
+        public long DefaultFeatureId { get; set; }
+
+        [ForeignKey("BacklogId")]
+        public Sprint Backlog { get; set; }
+
+        [ForeignKey("DefaultFeatureId")]
+        public Feature DefaultFeature { get; set; }
     }
 }
--- Scrumr/Views/EditViews/EditProject.cs
+++ Scrumr/Views/EditViews/EditProject.cs
@@ -10,5 +10,18 @@ namespace Scrumr
     {
         public EditProject(ScrumrContext context, Entity entity = null)
             : base(typeof(Project), context, entity) { }
+
+        protected override void OnCreating(Entity entity)
+        {
+            var project = entity as Project;
+            var feature = new Feature { Name = "General", Project = project };
+            var sprint = new Sprint { Name = "Backlog", Project = project };
+
+            Context.Features.Add(feature);
+            Context.Sprints.Add(sprint);
+
+            //project.DefaultFeature = feature;
+            //project.Backlog = sprint;
+        }
     }
 }
