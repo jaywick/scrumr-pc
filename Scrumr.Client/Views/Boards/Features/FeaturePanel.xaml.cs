51a59a763f4161b4ef73eaa34b4b1e4b0e2237a4
Jay Wick <developer@jay-wick.com>
2015-03-03 21:08:27 +1100

    Reorganised Features and Matrix view components. Saved and restored which feature panels were open/closed before and after updated.

---
+++ Scrumr.Client/Views/Boards/Features/FeaturePanel.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Scrumr.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scrumr.Client
+{
+    public partial class FeaturePanel : UserControl
+    {
+        public event Action Updated;
+
+        public Feature Feature { get; set; }
+
+        private ScrumrContext Context { get; set; }
+
+        public FeaturePanel()
+        {
+            InitializeComponent();
+        }
+
+        public FeaturePanel(ScrumrContext context, Feature feature)
+            : this()
+        {
+            Context = context;
+            Feature = feature;
+
+            foreach (var ticket in Feature.GetTickets(Context).OrderBy(x => x.SprintId))
+            {
+                var ticketView = new TileTicketView(ticket);
+
+                ticketView.RequestClose += (t) => CloseTicket(t as Ticket);
+                ticketView.RequestReopen += (t) => OpenTicket(t as Ticket);
+                ticketView.RequestEdit += (t) => EditTicket(t as Ticket);
+                ticketView.RequestRemove += (t) => RemoveEntity(t as Ticket);
+
+                LayoutRoot.Children.Add(ticketView);
+            }
+
+            var addTile = new AddButtonTileView(Feature, Feature.Project.Backlog);
+            addTile.Added += AddedTicket;
+            LayoutRoot.Children.Add(addTile);
+        }
+
+        private void OpenTicket(Ticket ticket)
+        {
+            ticket.Open();
+            Updated();
+        }
+
+        private void CloseTicket(Ticket ticket)
+        {
+            ticket.Close();
+            Updated();
+        }
+
+        private void EditTicket(Ticket ticket)
+        {
+            ViewDirector.EditTicket(ticket, Context);
+            Updated();
+        }
+
+        private void RemoveEntity<T>(T entity) where T : Entity
+        {
+            ViewDirector.RemoveEntity(entity, Context);
+            Updated();
+        }
+
+        public async void AddedTicket(Ticket ticket)
+        {
+            Context.Tickets.Add(ticket);
+            await Context.SaveChangesAsync();
+
+            Updated();
+        }
+
+        public void ToggleVisiblity()
+        {
+            if (this.Visibility == System.Windows.Visibility.Visible)
+                this.Visibility = System.Windows.Visibility.Collapsed;
+            else
+                this.Visibility = System.Windows.Visibility.Visible;
+        }
+    }
+}
---
+++ Scrumr.Client/Views/Boards/Features/FeatureView.xaml.cs
@@ -0,0 +1,114 @@
+﻿using Scrumr.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scrumr.Client.Views
+{
+    public partial class FeatureView : UserControl, IBoardView
+    {
+        public event Action<Project> OnProjectAdded;
+
+        public Database.ScrumrContext Context { get; set; }
+        public Database.Project Project { get; set; }
+
+        public FeatureView()
+        {
+            InitializeComponent();
+        }
+
+        public void NewSprint()
+        {
+            ViewDirector.AddEntity<Sprint>(Context, Project.ID);
+            Update();
+        }
+
+        public void NewFeature()
+        {
+            ViewDirector.AddEntity<Feature>(Context, Project.ID);
+            Update();
+        }
+
+        public void NewTicket()
+        {
+            ViewDirector.AddTicket(Context, Project.ID);
+            Update();
+        }
+
+        public void NewProject()
+        {
+            var project = ViewDirector.AddEntity<Project>(Context);
+            Update();
+
+            if (OnProjectAdded != null)
+                OnProjectAdded.Invoke(project);
+        }
+
+        public void Update()
+        {
+            SaveView();
+            RootItems.Children.Clear();
+            featureVisibilityMap.Clear();
+
+            foreach (var feature in Project.Features)
+            {
+                var featurePanel = new FeaturePanel(Context, feature);
+                var featureHeader = CreateFeatureHeader(feature);
+
+                featurePanel.Updated += () => Update();
+                featureHeader.PreviewMouseDown += (s, e) => ToggleVisibility(featurePanel);
+
+                RootItems.Children.Add(featureHeader);
+                RootItems.Children.Add(featurePanel);
+            }
+
+            RestoreView();
+        }
+
+        private void ToggleVisibility(FeaturePanel featurePanel)
+        {
+            featurePanel.ToggleVisiblity();
+        }
+
+        private UIElement CreateFeatureHeader(Database.Feature feature)
+        {
+            return new Label
+            {
+                Content = feature.Name,
+                FontSize = 16,
+                Foreground = Brushes.Black,
+            };
+        }
+
+        Dictionary<Feature, Visibility> featureVisibilityMap = new Dictionary<Feature,Visibility>();
+        private void SaveView()
+        {
+            foreach (var featurePanel in RootItems.Children.OfType<FeaturePanel>())
+            {
+                featureVisibilityMap.Add(featurePanel.Feature, featurePanel.Visibility);
+            }
+        }
+
+        private void RestoreView()
+        {
+            foreach (var featurePanel in RootItems.Children.OfType<FeaturePanel>())
+            {
+                if (featureVisibilityMap.ContainsKey(featurePanel.Feature))
+                    RootItems.Visibility = featureVisibilityMap[featurePanel.Feature];
+                else
+                    RootItems.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+    }
+}
